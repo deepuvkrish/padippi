e748adcdc1be3dce13b3561551549978bd210933
AkhilAki <akhilaki261097@gmail.com>
2019-04-02 20:03:06 +0530

    navbar changed

--- include/navbar.php
+++ include/navbar.php
@@ -1,8 +1,20 @@
 
-
+<style>
+.container-cs{
+    max-width: 120rem;
+    width: 90%;
+    margin: 0 auto;
+}
+.fixed-to{
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 1030;
+}
+</style>
     <header>
-        <div class="container">
-            <nav>
+        <div class="container-cs">
+            <nav class="navbar fixed-to navbar-expand-lg navbar-light bg-lig">
                 <h1 class="brand"><a href="index.php">pad<span>i</span>ppi</a></h1>
                 <ul>
                     <li><a href="index.php">Home</a></li>
